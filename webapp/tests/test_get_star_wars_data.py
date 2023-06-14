b6bfcd71f39a225e5b9a17b68557fb6d3e455881
Humphrey Chiou <koalaatb81dotorg@gmail.com>
2023-06-14 22:18:01 +0800

    add 3 tests (http_error, hello, top_people)

--- webapp/tests/test_get_star_wars_data.py
+++ webapp/tests/test_get_star_wars_data.py
@@ -8,4 +8,24 @@ def test_get_star_wars_data():
     assert response.status_code == 200
     assert "name" in response.json()
 
-# Additional test cases can be added by the candidate
+
+def test_api_http_error():
+    client = TestClient(app)
+    response = client.get("/data?num=65535")
+    assert response.status_code == 500
+    assert response.json()["detail"] == "API Error"
+
+
+def test_api_hello():
+    client = TestClient(app)
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "message" in response.json()
+
+
+def test_api_top_people():
+    client = TestClient(app)
+    response = client.get("/top-people-by-bmi")
+    assert response.status_code == 200
+    assert len(response.json()) == 20
+    assert "name" in response.json()[0]
